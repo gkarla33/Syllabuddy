ddc49322fa51aff1ab93a24ff7bf2332f251be90
gkarla33 <49798102+gkarla33@users.noreply.github.com>
2022-11-29 19:05:40 -0600

    Create app.js

---
+++ app.js
@@ -0,0 +1,34 @@
+const express = require("express");
+const bodyParser = require("body-parser");
+const router = express.Router();
+const app = express();
+
+app.use(
+  bodyParser.urlencoded({
+    extended: true,
+  })
+);
+app.use(bodyParser.json());
+
+app.use("/static", express.static(__dirname + "/static"));
+
+app.use("/", router);
+app.use("/schedule", router);
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(bodyParser.json());
+
+app.get("/", (req, res) => {
+  res.sendFile(__dirname + "/index.html");
+});
+
+app.get("/schedule", (req, res) => {
+  res.sendFile(__dirname + "/schedule.html");
+});
+
+app.post("/", (req, res) => {
+  res.redirect("/schedule");
+});
+
+app.listen(3000, () => {
+  console.log("Application started and Listening on port 3000");
+});
