2480b987d2bb665579daf06ec747a293319b0dac
gkarla33 <49798102+gkarla33@users.noreply.github.com>
2022-12-09 12:46:03 -0600

    Update app.js

--- app.js
+++ app.js
@@ -2,12 +2,27 @@ const express = require("express");
 const bodyParser = require("body-parser");
 const router = express.Router();
 const app = express();
+const cookieParser = require("cookie-parser");
+app.use(cookieParser());
+const bcrypt = require("bcrypt");
+const saltRounds = 5;
 
-app.use(
-  bodyParser.urlencoded({
-    extended: true,
-  })
-);
+const mysql = require("mysql");
+const res = require("express/lib/response");
+const con = mysql.createConnection({
+    user: "sql5582571",
+    password: "ec3txGsPs8",
+    host: "sql5.freesqldatabase.com",
+    database: "sql5582571",
+    port: "3306",
+});
+
+con.connect(function (err) {
+    if (err) throw err;
+    console.log("Connected!");
+});
+
+app.use( bodyParser.urlencoded({extended: true,}));
 app.use(bodyParser.json());
 
 app.use("/static", express.static(__dirname + "/static"));
@@ -20,20 +35,63 @@ app.use(bodyParser.urlencoded({ extended: false }));
 app.use(bodyParser.json());
 
 app.get("/", (req, res) => {
-  res.sendFile(__dirname + "/index.html");
+  res.sendFile(__dirname + "/public/login.html");
 });
 
 app.get("/schedule", (req, res) => {
-  res.sendFile(__dirname + "/schedule.html");
+  res.sendFile(__dirname + "/public/schedule.html");
 });
 
 app.get("/syllabus", (req, res) => {
-  res.sendFile(__dirname + "/syllabus.html");
+  res.sendFile(__dirname + "/public/syllabus.html");
 });
 
 app.post("/", (req, res) => {
-  res.redirect("/schedule");
+  console.log("checking user in database");
+
+  let username = req.body.username;
+  let password = req.body.password;
+
+  var sql = "SELECT * FROM User WHERE username = '" + username + "';";
+
+  con.query(sql, function (err, result) {
+    if (err) throw err;
+    if (result.length > 0) {
+      
+        sql = "SELECT password FROM User WHERE username = '" + username + "';";
+
+        con.query(sql, function (err, result) {
+            if (err) throw err;
+
+            let pass = result[0].password;
+            bcrypt.compare(password, pass, function(err, result) {
+                if (result) {
+                    console.log("Password is correct");
+                    res.redirect("/schedule");
+                }
+                else {
+                    console.log("Password is incorrect");
+                    res.redirect("/")
+                }
+            });
+        });
+    } 
+    else {
+      console.log("User not found, creating new profile");
+
+      bcrypt.hash(password, saltRounds, function(err, hash) {
+        sql = "INSERT INTO User (Username, Password) VALUES ('" + username + "', '" + hash + "');";
+
+      con.query(sql, function (err, result) {
+        console.log("New user added");
+      });
+    });
+  
+      res.redirect("/schedule");
+    }
+  });
 });
+
 
 app.post("/schedule", (req, res) => {
   res.redirect("/syllabus");
